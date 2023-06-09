57cc0f3fed45a589d041a354c3fbec4a0bf0e703
Allysson Oliveira <allysson.oliveira@capgemini.com>
2023-06-09 12:50:15 -0300

    colocado slides na tela de favoritos

--- src/app/favoritos/favoritos.page.ts
+++ src/app/favoritos/favoritos.page.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
+import { IonSlides } from '@ionic/angular';
 
 @Component({
   selector: 'app-favoritos',
@@ -11,13 +12,13 @@ export class FavoritosPage implements OnInit {
   eventoBotaoClickPolo: boolean = false;
   eventoBotaoClickBlocos: boolean = false;
   diaSemana = [
-    { dia: 'Home.dia.dia1', selecao: false, id: 1 },
-    { dia: 'Home.dia.dia2', selecao: false, id: 2 },
-    { dia: 'Home.dia.dia3', selecao: false, id: 3 },
-    { dia: 'Home.dia.dia4', selecao: false, id: 4 },
-    { dia: 'Home.dia.dia5', selecao: false, id: 5 },
-    { dia: 'Home.dia.dia6', selecao: false, id: 6 },
-    { dia: 'Home.dia.dia7', selecao: false, id: 7 },
+    { dia: 'Home.dia.dia1', selecao: "segment1", id: 1 },
+    { dia: 'Home.dia.dia2', selecao: "segment2", id: 2 },
+    { dia: 'Home.dia.dia3', selecao: "segment3", id: 3 },
+    { dia: 'Home.dia.dia4', selecao: "segment4", id: 4 },
+    { dia: 'Home.dia.dia5', selecao: "segment5", id: 5 },
+    { dia: 'Home.dia.dia6', selecao: "segment6", id: 6 },
+    { dia: 'Home.dia.dia7', selecao: "segment7", id: 7 },
   ];
 
   items = [
@@ -27,7 +28,7 @@ export class FavoritosPage implements OnInit {
       img: './assets/imagens/1.png',
       data: ' Qui, 09 Mar - 19:00',
       local: 'Sebrae - Recife',
-      favorito: false,
+      favorito: true,
     },
     {
       id: 2,
@@ -35,7 +36,7 @@ export class FavoritosPage implements OnInit {
       img: './assets/imagens/2.png',
       data: ' Qui, 09 Mar - 19:00',
       local: 'Teatro do Parque - Recife',
-      favorito: false,
+      favorito: true,
     },
     {
       id: 3,
@@ -43,7 +44,7 @@ export class FavoritosPage implements OnInit {
       img: './assets/imagens/3.jpg',
       data: ' Qui, 09 Mar - 19:00',
       local: 'Online',
-      favorito: false,
+      favorito: true,
     },
     {
       id: 4,
@@ -51,7 +52,7 @@ export class FavoritosPage implements OnInit {
       img: './assets/imagens/4.jpg',
       data: ' Qui, 09 Mar - 19:00',
       local: 'Sebrae - Recife',
-      favorito: false,
+      favorito: true,
     },
     {
       id: 5,
@@ -59,7 +60,7 @@ export class FavoritosPage implements OnInit {
       img: './assets/imagens/5.png',
       data: ' Qui, 09 Mar - 19:00',
       local: 'Sebrae - Recife',
-      favorito: false,
+      favorito: true,
     },
     {
       id: 6,
@@ -67,7 +68,7 @@ export class FavoritosPage implements OnInit {
       img: './assets/imagens/1.png',
       data: ' Qui, 09 Mar - 19:00',
       local: 'Sebrae - Recife',
-      favorito: false,
+      favorito: true,
     },
     {
       id: 7,
@@ -75,7 +76,7 @@ export class FavoritosPage implements OnInit {
       img: './assets/imagens/2.png',
       data: ' Qui, 09 Mar - 19:00',
       local: 'Teatro do Parque - Recife',
-      favorito: false,
+      favorito: true,
     },
     {
       id: 8,
@@ -83,7 +84,7 @@ export class FavoritosPage implements OnInit {
       img: './assets/imagens/3.jpg',
       data: ' Qui, 09 Mar - 19:00',
       local: 'Online',
-      favorito: false,
+      favorito: true,
     },
     {
       id: 9,
@@ -91,7 +92,7 @@ export class FavoritosPage implements OnInit {
       img: './assets/imagens/4.jpg',
       data: ' Qui, 09 Mar - 19:00',
       local: 'Sebrae - Recife',
-      favorito: false,
+      favorito: true,
     },
     {
       id: 10,
@@ -99,12 +100,28 @@ export class FavoritosPage implements OnInit {
       img: './assets/imagens/5.png',
       data: ' Qui, 09 Mar - 19:00',
       local: 'Sebrae - Recife',
-      favorito: false,
+      favorito: true,
     },
   ];
+
+
+  @ViewChild(IonSlides) slides!: IonSlides;
+
+  selectedSegment: string = " ";
+
+  slideOptions = {
+    allowSlidePrev: true,
+    allowSlideNext: true,
+
+
+  };
+
   constructor(private router: Router) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.selectedSegment = "segment1"
+
+  }
 
   favoritar(value: any) {
     value.favorito = !value.favorito;
@@ -115,31 +132,66 @@ export class FavoritosPage implements OnInit {
     this.router.navigate(['carnival/tabs/detalhes/' + id]);
   }
 
-  eventoBotao(i: any, item: any) {
-    this.diaSemana[i].selecao = !this.diaSemana[i].selecao;
-    console.log(item);
+  // eventoBotao(i: any, item: any) {
+  //   this.diaSemana[i].selecao = !this.diaSemana[i].selecao;
+  //   console.log(item);
 
-    for (let index = 0; index < this.diaSemana.length; index++) {
-      if (item.id != this.diaSemana[index].id) {
-        this.diaSemana[index].selecao = false;
-      }
-    }
-  }
+  //   for (let index = 0; index < this.diaSemana.length; index++) {
+  //     if (item.id != this.diaSemana[index].id) {
+  //       this.diaSemana[index].selecao = false;
+  //     }
+  //   }
+  // }
 
-  chamarPolosGerais(valor: any){
-    if(valor === 1){
-      this.eventoBotaoClickBlocos = true;
-      this.eventoBotaoClickPolo = false;
+  // chamarPolosGerais(valor: any){
+  //   if(valor === 1){
+  //     this.eventoBotaoClickBlocos = true;
+  //     this.eventoBotaoClickPolo = false;
 
 
-    } else if (valor === 2){
-      this.eventoBotaoClickPolo = true;
-      this.eventoBotaoClickBlocos = false;
+  //   } else if (valor === 2){
+  //     this.eventoBotaoClickPolo = true;
+  //     this.eventoBotaoClickBlocos = false;
 
-    }
-  }
+  //   }
+  // }
 
   imprimirDia(valor: any){
     console.log(valor)
+    console.log(this.selectedSegment);
+
+    // this.selectedSegment = valor.id;
+  }
+
+  onSegmentChange() {
+    this.slides.slideTo(this.getSegmentIndex());
+  }
+
+ onSlideChange = async () =>  {
+    const activeIndex = await this.slides.getActiveIndex(); // Aguarda a resolução da Promise
+    this.selectedSegment = 'segment' + (activeIndex + 1);
+
+ }
+
+  getSegmentIndex(): number {
+
+    switch (this.selectedSegment) {
+      case 'segment1':
+        return 0;
+      case 'segment2':
+        return 1;
+      case 'segment3':
+        return 2;
+      case 'segment4':
+        return 3;
+      case 'segment5':
+        return 4;
+      case 'segment6':
+        return 5;
+      case 'segment7':
+        return 6;
+
+    }
+    return 0;
   }
 }
